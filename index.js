c75503ae9092a31ebc2b5a58d763ff43242dbea8
Adolphe1501 <100210115+Adolphe1501@users.noreply.github.com>
2023-02-11 00:27:38 +0000

    completing shorturl creation redirection and verification of an posted

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+let bodyParser = require("body-parser");
+var urls = {};
 
 // Basic Configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = process.env.PORT || 3000;
 
 app.use(cors());
@@ -17,6 +21,34 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+
+//redirect short url
+app.get('/api/shorturl/:shorturl', function(req, res) {
+  res.statusCode = 302;
+  res.setHeader("Location", urls[req.params.shorturl]);
+  res.end();
+});
+
+// post url and send back url and short url
+function isValidHttpUrl(string) {
+  try {
+    const newUrl = new URL(string);
+    return newUrl.protocol === 'http:' || newUrl.protocol === 'https:';
+  } catch (err) {
+    return false;
+  }
+}
+app.post('/api/shorturl', function(req, res) {
+  if(isValidHttpUrl(req.body.url))
+  {
+    console.log(Object.keys(urls).length);
+    urls[Object.keys(urls).length]=req.body.url;
+    console.log(urls);
+    res.json({ original_url: req.body.url , short_url: Object.keys(urls).length-1 });
+  }
+  else
+    res.json({error :'invalid url' });
 });
 
 app.listen(port, function() {
